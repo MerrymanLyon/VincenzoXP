9e63b5797d3d9c092d20016a4f20d1c5a4c4ace1
MerrymanLyon <108525061+MerrymanLyon@users.noreply.github.com>
2026-09-13 11:32:18 +0200

    fix colour recycling bin 2

--- src/programs/RecyclingBin.tsx
+++ src/programs/RecyclingBin.tsx
@@ -6,37 +6,52 @@ interface RecyclingBinProps {
 
 const RecyclingBin: React.FC<RecyclingBinProps> = () => {
   return (
-    <div
-      style={{
-        width: "100%",
-        height: "100%",
-        minHeight: "300px",
-        backgroundColor: "#ffffff",
-        padding: "20px",
-        boxSizing: "border-box",
-        overflowY: "auto",
-        userSelect: "text",
-        WebkitUserSelect: "text",
-        cursor: "default",
-      }}
-    >
-      <p
+    <>
+      <style jsx>{`
+        .secretText {
+          color: #ffffff;
+          background-color: #ffffff;
+          margin: 0;
+          font-size: 13px;
+          line-height: 1.6;
+          font-family: Tahoma, sans-serif;
+          user-select: text;
+          -webkit-user-select: text;
+        }
+
+        /* DEFINIZIONE DEL COLORE TESTO QUANDO VIENE SELEZIONATO DAL MOUSE */
+        .secretText::selection {
+          background-color: #0a246a; /* Blu classico selezione Windows XP */
+          color: #ffffff; /* Testo bianco ben visibile su fondo blu */
+        }
+
+        .secretText::-moz-selection {
+          background-color: #0a246a;
+          color: #ffffff;
+        }
+      `}</style>
+
+      <div
         style={{
-          color: "#ffffff",
+          width: "100%",
+          height: "100%",
+          minHeight: "300px",
           backgroundColor: "#ffffff",
-          margin: 0,
-          fontSize: "13px",
-          lineHeight: "1.6",
-          fontFamily: "Tahoma, sans-serif",
+          padding: "20px",
+          boxSizing: "border-box",
+          overflowY: "auto",
+          cursor: "default",
         }}
       >
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lobortis
-        quam a ex sagittis pharetra. Etiam gravida blandit eros nec rhoncus.
-        Maecenas aliquet bibendum ligula. Vivamus non massa enim. Maecenas
-        bibendum quis sapien vitae blandit. Curabitur imperdiet fermentum purus
-        sit amet mollis.
-      </p>
-    </div>
+        <p className="secretText">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lobortis
+          quam a ex sagittis pharetra. Etiam gravida blandit eros nec rhoncus.
+          Maecenas aliquet bibendum ligula. Vivamus non massa enim. Maecenas
+          bibendum quis sapien vitae blandit. Curabitur imperdiet fermentum purus
+          sit amet mollis.
+        </p>
+      </div>
+    </>
   );
 };
 
